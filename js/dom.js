ab436b0e97b8b25b0dfb88ea62e5d4c58877343a
AkibRiad <akibriad652@gmail.com>
2024-10-14 21:16:01 +0600

    donation btn % history button complete

--- js/dom.js
+++ js/dom.js
@@ -17,7 +17,18 @@ document.getElementById('donate-btn-one').addEventListener('click',function(even
     const newNoakhaliBalance = noakhaliBalance + inputOne;
     document.getElementById('main-balance').innerText = newmainBalance;
     document.getElementById('noakhali-balance').innerText=newNoakhaliBalance;
+
+    const historyContainer = document.createElement('div');
+    historyContainer.className= 'bg-white shadow-lg rounded-lg p-4 border-2 border-cyan-300';
     
+    historyContainer.innerHTML=`
+    <p>${inputOne.toFixed(2)} Taka is Donate for Flood at Noakhali,Bangladesh</p>
+    <p>${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}</p>  
+    `
+    const historyList =document.getElementById('history-list');
+    historyList.appendChild(historyContainer)
+    
+
 })
 
 //  feni section 
@@ -31,6 +42,16 @@ document.getElementById('donate-btn-two').addEventListener('click',function(even
     const newfeniBalance = feniBalance + inputTwo;
     document.getElementById('main-balance').innerText = newmainBalance;
     document.getElementById('feni-balance').innerText=newfeniBalance;
+
+    const historyContainer = document.createElement('div');
+    historyContainer.className= 'bg-white shadow-lg rounded-lg p-4 border-2 border-cyan-300';
+    
+    historyContainer.innerHTML=`
+    <p>${inputTwo.toFixed(2)} Taka is Donate for Flood Relief in Feni,Bangladeshh</p>
+    <p>${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}</p>  
+    `
+    const historyList =document.getElementById('history-list');
+    historyList.appendChild(historyContainer)
     
 })
 
@@ -46,15 +67,45 @@ document.getElementById('donate-btn-three').addEventListener('click',function(ev
     const newQuotaBalance = quotaBalance + inputThree;
     document.getElementById('main-balance').innerText = newmainBalance;
     document.getElementById('quota-balance').innerText=newQuotaBalance;
+
+    const historyContainer = document.createElement('div');
+    historyContainer.className= ' bg-white shadow-lg rounded-lg p-4 border-2 border-cyan-300';
+    
+    historyContainer.innerHTML=`
+    <p>${inputThree.toFixed(2)} Taka is Donate for Injured in the Quota Movement</p>
+    <p>${new Date().toLocaleDateString()} ${new Date().toLocaleTimeString()}</p>  
+    `
+    const historyList =document.getElementById('history-list');
+    historyList.appendChild(historyContainer)
     
 })
 
 
-// donate button section 
+// history button section 
  document.getElementById('history').addEventListener('click',function(event){
     event.preventDefault();
     document.getElementById('donation').classList.remove("bg-cyan-500");
     document.getElementById('history').classList.add("bg-cyan-500");
-    console.log(999999)
+    
+
+    const historyList = document.getElementById('history-list');
+    historyList.classList.remove('hidden');
+    const mainBody = document.getElementById('main-body');
+    mainBody.classList.add('hidden');
  })
+
+
+//  donation button 
+
+document.getElementById('donation').addEventListener('click',function(event){
+event.preventDefault();
+const historyList = document.getElementById('history-list');
+    historyList.classList.add('hidden');
+    const mainBody = document.getElementById('main-body');
+    mainBody.classList.remove('hidden');
+
+    document.getElementById('history').classList.remove("bg-cyan-500");
+    document.getElementById('donation').classList.add("bg-cyan-500");
+
+})
 
