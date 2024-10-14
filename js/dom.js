029a007fcd7cbff2c763a410a76e6da544476f93
AkibRiad <akibriad652@gmail.com>
2024-10-14 22:40:27 +0600

    added function & complete this assignment

--- js/dom.js
+++ js/dom.js
@@ -1,3 +1,20 @@
+// innervalue getting function 
+function getInputValueById(id){
+     return parseFloat(document.getElementById(id).value);
+    
+}
+// innerText getting function 
+function getInputTextById(id) {
+    return parseFloat(document.getElementById(id).innerText); 
+}
+
+// remove hidden function 
+function showInvalid(id){
+    return document.getElementById(id).classList.remove('hidden');
+}
+
+
+
 // blog button section 
 document.getElementById('blog-btn').addEventListener('click',function(){
     window.location.href='blog.html';
@@ -9,20 +26,31 @@ document.getElementById('blog-btn').addEventListener('click',function(){
 // noakhali button section 
 document.getElementById('donate-btn-one').addEventListener('click',function(event){
     event.preventDefault();
-    const mainbalance = parseFloat(document.getElementById('main-balance').innerText);
-    const inputOne = parseFloat(document.getElementById('input-one').value);
-    const noakhaliBalance = parseFloat(document.getElementById('noakhali-balance').innerText);
+    // const mainbalance = parseFloat(document.getElementById('main-balance').innerText);
+    // const inputOne = parseFloat(document.getElementById('input-one').value);
+    // const noakhaliBalance = parseFloat(document.getElementById('noakhali-balance').innerText);
 
+    // calling function 
+    const mainBalance = getInputTextById('main-balance');
+    
+    const inputOne = getInputValueById("input-one");
+    
+    const noakhaliBalance = getInputTextById("noakhali-balance");
+    
+
+    
     if(inputOne <= 0 || isNaN(inputOne)){
-        document.getElementById('invalid-one').classList.remove("hidden");
+        // document.getElementById('invalid-one').classList.remove("hidden");
+        // calling hidden function 
+        showInvalid("invalid-one");
         return;
     }
-    if(inputOne > mainbalance){
+    if(inputOne > mainBalance){
         alert("You Don't Have Enough Money For Donate");
         return;
     }
 
-    const newmainBalance = mainbalance - inputOne;
+    const newmainBalance = mainBalance - inputOne;
     const newNoakhaliBalance = noakhaliBalance + inputOne;
     document.getElementById('main-balance').innerText = newmainBalance;
     document.getElementById('noakhali-balance').innerText=newNoakhaliBalance;
@@ -43,23 +71,32 @@ document.getElementById('donate-btn-one').addEventListener('click',function(even
 //  feni section 
 document.getElementById('donate-btn-two').addEventListener('click',function(event){
     event.preventDefault();
-    const mainbalance = parseFloat(document.getElementById('main-balance').innerText);
-    const inputTwo = parseFloat(document.getElementById('input-two').value);
-    const feniBalance = parseFloat(document.getElementById('feni-balance').innerText);
+    // const mainbalance = parseFloat(document.getElementById('main-balance').innerText);
+    // const inputTwo = parseFloat(document.getElementById('input-two').value);
+    // const feniBalance = parseFloat(document.getElementById('feni-balance').innerText);
+    // calling function
+    const mainBalance = getInputTextById('main-balance');
+    
+    const inputTwo = getInputValueById("input-two");
+    
+    const feniBalance = getInputTextById("feni-balance");
+    
 
     if(inputTwo <= 0 || isNaN(inputTwo)){
-        document.getElementById('invalid-two').classList.remove("hidden");
+        // document.getElementById('invalid-two').classList.remove("hidden");
+        // calling hidden function 
+        showInvalid("invalid-two");
         return;
     }
-    if(inputTwo > mainbalance){
+    if(inputTwo > mainBalance){
         alert("You Don't Have Enough Money For Donate");
         return;
     }
 
-    const newmainBalance = mainbalance - inputTwo;
-    const newfeniBalance = feniBalance + inputTwo;
+    const newmainBalance = mainBalance - inputTwo;
+    const newFeniBalance = feniBalance + inputTwo;
     document.getElementById('main-balance').innerText = newmainBalance;
-    document.getElementById('feni-balance').innerText=newfeniBalance;
+    document.getElementById('feni-balance').innerText=newFeniBalance;
 
     const historyContainer = document.createElement('div');
     historyContainer.className= 'bg-white shadow-lg rounded-lg p-4 border-2 border-cyan-300';
@@ -77,20 +114,30 @@ document.getElementById('donate-btn-two').addEventListener('click',function(even
 // quota section 
 document.getElementById('donate-btn-three').addEventListener('click',function(event){
     event.preventDefault();
-    const mainbalance = parseFloat(document.getElementById('main-balance').innerText);
-    const inputThree = parseFloat(document.getElementById('input-three').value);
-    const quotaBalance = parseFloat(document.getElementById('quota-balance').innerText);
+    // const mainbalance = parseFloat(document.getElementById('main-balance').innerText);
+    // const inputThree = parseFloat(document.getElementById('input-three').value);
+    // const quotaBalance = parseFloat(document.getElementById('quota-balance').innerText);
+
+    // calling function
+    const mainBalance = getInputTextById('main-balance');
+    
+    const inputThree = getInputValueById("input-three");
+    
+    const quotaBalance = getInputTextById("quota-balance");
+    
 
     if(inputThree <= 0 || isNaN(inputThree)){
-        document.getElementById('invalid-three').classList.remove("hidden");
+        // document.getElementById('invalid-three').classList.remove("hidden");
+        // calling hidden function 
+        showInvalid("invalid-three");
         return;
     }
-    if(inputThree > mainbalance){
+    if(inputThree > mainBalance){
         alert("You Don't Have Enough Money For Donate");
         return;
     }
 
-    const newmainBalance = mainbalance - inputThree;
+    const newmainBalance = mainBalance - inputThree;
     const newQuotaBalance = quotaBalance + inputThree;
     document.getElementById('main-balance').innerText = newmainBalance;
     document.getElementById('quota-balance').innerText=newQuotaBalance;
